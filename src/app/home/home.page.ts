e359cf939909bb8d9329175fcd78a5034766ce2c
Lucas Renó <lucasreno9@gmail.com>
2022-09-16 21:18:00 -0300

    Projeto funcional

---
+++ src/app/home/home.page.ts
@@ -0,0 +1,22 @@
+import { HttpClient } from '@angular/common/http';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: 'home.page.html',
+  styleUrls: ['home.page.scss'],
+})
+export class HomePage {
+  nome1: string;
+  nome2: string;
+  porcentagem: number;
+
+  constructor(private http: HttpClient) { }
+
+  calcular() {
+    let url = 'http://lucasreno.kinghost.net/love-calculator/';
+    url = url + this.nome1 + '/' + this.nome2;
+
+    this.http.get(url).subscribe(resposta => this.porcentagem = +resposta);
+  }
+}
